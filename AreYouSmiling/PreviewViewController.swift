19c3a7d9e396e44d5f27837b7969b511a4a0defb
ahmed3m <ahmed_3m2010@hotmail.com>
2016-07-06 22:14:10 -0400

    Create the ability to view the selected picture in the PreviewViewController

--- AreYouSmiling/PreviewViewController.swift
+++ AreYouSmiling/PreviewViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
+import AlamofireImage
+import AlamofireNetworkActivityIndicator
 
 class PreviewViewController: UIViewController {
 
     var photoTakingHelper: PhotoTakingHelper?
+    var photos: [UIImage] = []
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /*
+        let apiToContact = "https://api.projectoxford.ai/emotion/v1.0/recognize"
+        Alamofire.request(.GET, apiToContact).validate().responseJSON() { response in
+            switch response.result {
+            case .Success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    print(json)
+                }
+            case .Failure(let error):
+                print(error)
+            }
+        }
+         */
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,9 +43,24 @@ class PreviewViewController: UIViewController {
 
     @IBAction func cameraButtonPressed(sender: AnyObject) {
         photoTakingHelper = PhotoTakingHelper(viewController: self) { (image: UIImage?) in
-            print("Received an image")
+            // take image and pass it to the api, and recieve info
+            self.photos.append(image!)
+            self.tableView.reloadData()
         }
     }
 
 }
 
+extension PreviewViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(photos.count)
+        return photos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("previewCell", forIndexPath: indexPath) as! PreviewTableViewCell
+        cell.previewImageView.image = photos[indexPath.row]
+        return cell
+    }
+}
